af2917ec01c25302393cecdde109ad760a39a65a
Marta Enachioiu <marta.enachioiu@gmail.com>
2022-10-17 17:28:04 +0300

    Create PostMapping and RequestBody

--- src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
+++ src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
@@ -1,10 +1,8 @@
 package net.javaguides.springbootrestapi.controller;
 
 import net.javaguides.springbootrestapi.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,5 +48,16 @@ public class StudentController {
         return new Student(id, firstName, lastName);
     }
 
+    // Spring boot REST API that handles HTTP POST Request
+    // @PostMapping and @RequestBody
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
 
 }
